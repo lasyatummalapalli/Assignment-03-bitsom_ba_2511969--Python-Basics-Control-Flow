3fce8a2b1dccdd6bddf542bbdab0a43fde0c7c46
lasyatummalapalli <lasyatummalapalli@gmail.com>
2026-03-31 16:28:14 +0530

    Created using Colab

--- part1_grade_tracker.py
+++ part1_grade_tracker.py
@@ -1,1 +1,39 @@
-
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNBhYDK5OY6f8G4IJd8hBjf",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/lasyatummalapalli/Assignment-03-bitsom_ba_2511969--Python-Basics-Control-Flow/blob/main/part1_grade_tracker.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "GHRl9XRhiZZ-"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
